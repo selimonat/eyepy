8012847bc4cb518501a0e26c2e5bfad9d178c7c6
Selim Onat <onatselim@gmail.com>
2018-06-26 16:38:07 +0200

    commenting

--- eyepy.py
+++ eyepy.py
@@ -17,13 +17,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt    
 #from pyedfread import edf
-
+def __init__():
+    1
 def get_filelist(search_pattern):
     '''    
-    Returns a list of tuples that stores path and metadeta about EDF files found 
-    in the SEARCH_PATTERN.
+    Returns a list of tuples that stores path and metadeta about the EDF files found 
+    in the SEARCH_PATTERN (the path where your project resides).
     
-    For each EDF file found tuple contains its path, participant and run number.
+    For each EDF file, the output tuple contains its path and metadata extracted 
+    from the path string. Metadata here means participant and run ids.
     
     Metadata is recovered using the nested get_subject and get_run functions. 
     You might need to change those depending on how you stored your data. In 
@@ -55,17 +57,21 @@ def get_filelist(search_pattern):
         filelist += [(edf_path,{'subject': int(get_subject()), 'run': int(get_run())})]
     return filelist
 
-def get_fixmat(filelist,filter_fun=None):
+def get_df(filelist,filter_fun=None):
     '''
-    Reads all EDF files with pyedfread and returns a merged dataframe.
+    Reads all EDF files in the filelist (see get_filelist) tuple with pyedfread 
+    and returns a merged dataframe where each row is one fixation.
     
-    Final dataframe consists of Event and Message dataframes (outputs of pyedfread).
+    The output dataframe consists of Event and Message dataframes (outputs of 
+    pyedfread module, see pyedfread). 
     
-    Metadata is used to label DataFrames from individual subjects and runs (see get_filelist).
+    Metadata is used to label each row with participant and run id.
+    (see get_filelist).
     
-    If no FILTER_FUN is provided, Message DataFrame is directly appended as is to events. 
-    In some cases one might desire to preprocess Message DataFrame before appending. 
-    In that case, the result of FILTER_FUN(messages) is appended.
+    If no FILTER_FUN is provided, Message DataFrame is directly appended to 
+    Event DataFrame. In some cases one might desire to preprocess Message 
+    DataFrame before appending. In that case, the result of FILTER_FUN(messages) 
+    is appended.
         
     See get_filelist to know more about filelist format.
     See pyedfread for more information on the output dataframes.
@@ -98,10 +104,10 @@ def get_fixmat(filelist,filter_fun=None):
         E             = E[list(file[1].keys())+['trial','gavx','gavy','start','end']]
         #get SYNCTIME (i.e. stim onsets) 
         E             = E.merge(M.loc[:,['SYNCTIME', 'py_trial_marker']], right_on='py_trial_marker',left_on='trial',how="left")
-            #correct time stamps with SYNCTIME
+        #correct time stamps with SYNCTIME
         E.start       = E.start - E.SYNCTIME
         E.end         = E.end   - E.SYNCTIME
-            #remove prestimulus fixations
+        #remove prestimulus fixations
         E             = E[E.start > 0]
         #index fixations
         E             = E.groupby("trial").apply(addfix)
@@ -109,10 +115,10 @@ def get_fixmat(filelist,filter_fun=None):
         E             = E.append(M.loc[M.py_trial_marker == -1,['DISPLAY_COORDS', 'validation_result:','subject','run']])                
         #drop useless columns
         E             = E.drop(['SYNCTIME','py_trial_marker'],axis=1)
-        #get what we want from messages
+        #get what we want from messages using filter_fun or simply discard messages.
         if filter_fun != None:
             m = filter_fun(M)
-            e[i]          = E.merge(m,left_on='trial',right_on='py_trial_marker',how='left')
+            e[i]  = E.merge(m,left_on='trial',right_on='py_trial_marker',how='left')
         else:
             e[i]  = E
         
@@ -133,8 +139,8 @@ def get_fixmat(filelist,filter_fun=None):
 
 def sanity_checks(df):
     """
-    conducts sanity checks on the data and reports. (1) calibration error. (2)
-    number of trials.
+    Conducts sanity checks on the data and reports. (1) calibration error. (2)
+    number of trials. DF must come from get_df function.
     """
     
     #check for calibration quality
@@ -150,7 +156,7 @@ def sanity_checks(df):
     plt.title('Calibration Quality')
     ymin, ymax = plt.ylim()
     plt.plot(np.repeat(np.mean(error),2),[ymin, ymax])
-    plt.box('off')
+    plt.box('off')    
     
     report = "There are {} participants ({}) with a calibration error\nthat is more than \
     half a degree. You might consider\nexcluding them from the analysis".format(len(bad),bad.values)
@@ -162,11 +168,11 @@ def sanity_checks(df):
     plt.xlabel('Conditions')    
     plt.ylabel('Participants')
     plt.colorbar()
-    plt.title('#Fixations per Condition')
+    plt.title('#Fixations per Condition')    
     #check for fixations outside the stimulus range.
     #add scatter matrix with x,y values per subject
     colors = df['subject'].transform(lambda x: (x-min(x))/(max(x)-min(x))).transform(lambda x: [x,0,1-x])
-    pd.tools.plotting.scatter_matrix(df[['gavx','gavy','fix']],c=colors,figsize=(15, 15), diagonal='hist',alpha=0.3,marker='o')
+    pd.tools.plotting.scatter_matrix(df[['gavx','gavy','fix']],c=colors,figsize=(15, 15), diagonal='hist',alpha=0.3,marker='o')    
 	#check whether all fixation have the same stimulus size
     stimulus_size    
     
@@ -180,31 +186,42 @@ def fdm(df,downsample=100,stim_size=[0,0,1599,1199]):
         DOWNSAMPLE is used to calculate the number of bins using
         bin = stim_size/downsample.
         
-        Returns a 2D dataframe where each cell represents fixation counts at that 
+        Returns a 2D DataFrame where each cell represents fixation counts at that 
         spatial location.
         
         Example:
-            eyepy.fdm(df)            
+           fdm =  eyepy.fdm(df)            
     '''    
     stim_size = np.array(stim_size)
     fdm_range = stim_size.reshape(2,2).T        #size of the count matrix
     fdm_bins  = (stim_size[[2,3]]+1 )/downsample #number of bins    
-    fdm, xedges, yedges, bla = plt.hist2d(df["gavx"],df["gavy"],range=fdm_range,bins=fdm_bins)
+    
+    print(stim_size)
+    print(fdm_range)
+    print(fdm_bins)
+    
+    fdm, xedges, yedges, bla = plt.hist2d(df["gavx"].values,
+                                          df["gavy"].values,
+                                          range=fdm_range,
+                                          bins=fdm_bins)
     return pd.DataFrame(fdm)
     #return fdm, xedges, yedges
     
 def plot(df,path_stim='',downsample=100):
     '''
-        Plots fixation counts and optinally overlays on the stimulus"
+        Plots fixation counts and optionally overlays on the stimulus"
     '''
     #solve the problem with spyder that prevents two images to be overlaid.
-    img=plt.imread(path_stim)
-    plt.imshow(img,alpha=.5)    
+    if path_stim != '':
+        img = plt.imread(path_stim)
+        plt.imshow(img,alpha=.5)    
     
-    count,x,y = fdm(df,downsample)
+    count = fdm(df,downsample)
+    x     = fdm.index.values[0], fdm.index.values[-1]
+    y     = fdm.columns.values[0], fdm.columns.values[-1]
     plt.imshow(count.T,extent=[x[0],x[-1],y[0],y[-1]],alpha=.5)    
     plt.show()    
-#        
+    
 def stimulus_size(df):
     '''
         Checks whether stimulus sizes are consistent across participants. Throws 
@@ -217,6 +234,23 @@ def stimulus_size(df):
     else:
         print("DataFrame contains heterogenous set of stimulus sizes!")
         raise SystemExit
+
+def get_data(df):
+    '''
+    returns FDM data as [sample,feature] matrix together with labels.
+    '''
+    G         = df.groupby(['subject','trial'])
+    t_sample  = len(G)
+    t_feature = len(fdm(G.first()).unstack())
+    data      = np.zeros([t_sample,t_feature])
+    label     = np.zeros([t_sample,1])
+    c = 0;
+    for name,group in G:        
+        label[c] = name[0]
+        data[c,] = fdm(group).unstack()
+        c=c+1;
+    return data,label
+
 
 
 def pattern_similarity(df):
@@ -242,6 +276,7 @@ def PCA(df,groupby,explained_variance=.95,downsample=100):
     return pca
 
 from sklearn.base import BaseEstimator, TransformerMixin
+
 class FDM_generator(BaseEstimator, TransformerMixin):
     '''
     Generates fixation density maps with a resolution as hyper-parameter.
